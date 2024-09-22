49edad9563cc95118cfd395baf07004e8c54b692
William Holtsdalen <williamlh@hotmail.no>
2024-09-22 18:43:46 +0200

    Add models for bank-application

--- core/models.py
+++ core/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    frist_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email =models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class BankAccount(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='accounts')
+    account_number = models.CharField(max_length=10, unique=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def deposit(self, amount):
+        self.balance += amount
+        self.save()
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            self.save()
+            return True
+        return False
+
+    def __str__(self):
+        return f'Konto {self.account_number} - Saldo: {self.balance}'
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE_CHOICES = [
+        ('deposit', 'Innskudd'),
+        ('withdraw', 'Uttak'),
+    ]
+
+    account = models.ForeignKey(BankAccount, on_delete=models.CASCADE, related_name='transactions')
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE_CHOICES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.transaction_type.capitalize()} på {self.amount} for konto {self.account.account_number}'
